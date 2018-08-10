60e6a6c90c8406107fb29c5ea2bff831a92ec0f2
JuanG <juangiacosabrk@gmail.com>
2018-08-10 14:54:20 -0300

    Estilizado el componente Person y el boton de app.js 	Estilizado Person con creacion de archivo .css en Person 	Estilizado el boton con in-line styling en render() dentro de App.js

--- src/App.js
+++ src/App.js
@@ -19,16 +19,45 @@ class App extends Component {
         {name: 'Luchas', age: 18}
       ]
     })
-
   }
+
+  changedNameHandler = (event) =>{
+    this.setState({
+      personaas: [
+        {name: "Juan", age:25},
+        {name: event.target.value, age: 312},
+        {name: 'Luchas', age: 18}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding: '8px',
+      cursor:'pointer',
+      margin:'auto'
+    };
+
     return (
       <div className="App">
-        <button onClick={ () => this.switchNameHandler("Luchiano")}>Switch name</button>
-        <h1>Hola {this.state.personaas[0].name} de {this.state.personaas[0].age} anos</h1>
-        <h1>Hola {this.state.personaas[1].name} de {this.state.personaas[1].age} anos</h1>
-        <h1>Hola {this.state.personaas[2].name} de {this.state.personaas[2].age} anos</h1>
-        <Person name="Juan" age="25" click={this.switchNameHandler.bind(this, "Juanete")}/>
+        <button
+         onClick={ () => this.switchNameHandler("Luchiano")}
+         style={style}
+        >Switch name</button>
+        <Person
+          name={this.state.personaas[0].name}
+         age={this.state.personaas[0].age}/>
+        <Person
+          name={this.state.personaas[1].name} 
+          age="25" 
+          click={this.switchNameHandler.bind(this, "Juanete")}
+          changed={this.changedNameHandler}/>
+         <Person
+          name={this.state.personaas[2].name}
+         age={this.state.personaas[2].age}/>
         
 
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css';
 const person = (props)=> {
 
     return(
-        <div>
-            <p onClick={props.click}>Hola mi nombre es {props.name} y tengo {props.age} años</p>
+        <div className="Person">
+            <p onClick={props.click} >Hola mi nombre es {props.name} y tengo {props.age} años</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
 
     )
