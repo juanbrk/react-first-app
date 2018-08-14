265f3c7648d1b21668b8abf675099cbacca1062e
JuanG <juangiacosabrk@gmail.com>
2018-08-13 21:43:28 -0300

    Aplicado el cambio dinámico de componentes en la aplicacion usando un operador ternario mostrando y ocultando componentes al clickear un boton.

--- src/App.js
+++ src/App.js
@@ -27,8 +27,16 @@ class App extends Component {
         {name: "Juan", age:25},
         {name: event.target.value, age: 312},
         {name: 'Luchas', age: 18}
-      ]
+      ],
+      showPersons: false
     })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    //Cambiamos el valor de showPersons del state. Este bloque cambia unicamente el valor de showPersons
+    //El resto del state se mantiene intacto
+    this.setState({showPersons: !doesShow})
   }
 
   render() {
@@ -41,23 +49,34 @@ class App extends Component {
       margin:'auto'
     };
 
+    //Se puede ejecutar codigo javascript al wrapear en {} a todo aquello que sea dinamico y pueda cambiar
+    //En este caso agrupamos a todos los <Person> en un div y aplicamos un operador ternario para ejecutar
+    //un cambio condicional. No se permite usar bloques if dentro del codigo. Se usa un ternary 
+    //Sintaxis del ternary condicion ? ejecutaSiTrue :  ejecutaSiElse
     return (
       <div className="App">
         <button
-         onClick={ () => this.switchNameHandler("Luchiano")}
+         onClick={this.togglePersonsHandler}
          style={style}
         >Switch name</button>
-        <Person
-          name={this.state.personaas[0].name}
-         age={this.state.personaas[0].age}/>
-        <Person
-          name={this.state.personaas[1].name} 
-          age="25" 
-          click={this.switchNameHandler.bind(this, "Juanete")}
-          changed={this.changedNameHandler}/>
-         <Person
-          name={this.state.personaas[2].name}
-         age={this.state.personaas[2].age}/>
+        
+        {
+          this.state.showPersons ? 
+          <div>
+            <Person
+              name={this.state.personaas[0].name}
+              age={this.state.personaas[0].age}/>
+            <Person
+              name={this.state.personaas[1].name} 
+              age="25" 
+              click={this.switchNameHandler.bind(this, "Juanete")}
+              changed={this.changedNameHandler}/>
+            <Person
+              name={this.state.personaas[2].name} 
+              age={this.state.personaas[2].age}/>
+        </div> : null
+        }
+        
         
 
       </div>
