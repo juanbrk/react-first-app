5f78bbdee711cd68c57a884658fc842d426f2d3a
JuanG <juangiacosabrk@gmail.com>
2018-08-14 12:17:35 -0300

    La modificacion del nombre de cada <Person en App.js se hace dinámicamente usando this.nameChangedHandler(event, person.id). Esto permite actualizar el estadod el componente solo para el nombre que ha sido cambiado y no todos.

--- src/App.js
+++ src/App.js
@@ -5,29 +5,34 @@ import Person from './Person/Person.js';
 class App extends Component {
   state = {
     personaas: [
-      {name: 'Juan', age:25},
-      {name: 'Luis', age: 32},
-      {name: 'Lucas', age: 18}
+      {id:'fa49s8',name: 'Juan', age:25},
+      {id:'a5e46s',name: 'Luis', age: 32},
+      {id:'e7a86',name: 'Lucas', age: 18}
     ]
   };
 
-  switchNameHandler = (newName) => {
+  //Usamos el metodo para cambiar el nombre de una persona, cambiando el estado del componente. 
+  switchNameHandler = (event, id) => {
+    
+    //Busco el indice de la persona a cambiar el nombre
+    const personIndex = this.state.personaas.findIndex(p => {
+      return p.id === id;
+    });
+    //creo un nuevo objeto para evitar valor por referencia. Uso el operador spread para que el objeto tenga
+    //los mismos atributos que la persona que obtuvimos de la array.  
+    const person = {...this.state.personaas[personIndex]};
+    
+    //Actualizo el nombre de la persona fetcheada, puedo hacerlo porque la array es distinta a la del estado
+    person.name = event.target.value;
+
+    //Actualizo la array y el estado del componente
+    const persons = [...this.state.personaas];
+    persons[personIndex] = person;
     this.setState({
-      personaas: [
-        {name: newName, age:25},
-        {name: 'Lufis', age: 312},
-        {name: 'Luchas', age: 18}
-      ]
+      personaas: persons 
     })
   } 
-
-  // usar el operador = entre arrays las copia por referencia. Lo que queremos que se haga es una nueva
-  //array, eso se hace usando el operador ES6 spread ... que vierte el contenido de la antigua array en
-  // una nueva. Esto se hace así porque siempre se debe mantener la inmutabilidad del estado del componente
-  // y una array copiada por referencia no es la mejor manera de hacerlo. 
   deletePersonHandler = (indicePersona) =>{
-    //const personas = this.state.personaas.slice();
-    //Esta es la forma antigua de hacerlo, es equivalente a utilizar spread
     const personas = [...this.state.personaas];
     personas.splice(indicePersona, 1);
     this.setState({personaas: personas});
@@ -49,9 +54,12 @@ class App extends Component {
             this.state.personaas.map((persona, indice) => {
             return (
               <Person
+              key={persona.id}
+              click={() => this.deletePersonHandler(indice)}
               name={persona.name} 
               age={persona.age}
-              click={() => this.deletePersonHandler(indice)} />
+              changed={(event)=> this.switchNameHandler(event, persona.id)}
+               />
             );
           })}
           </div> 
