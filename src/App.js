ec546f971d0f80c347704745eb9666a3ff5d018d
JuanG <juangiacosabrk@gmail.com>
2018-08-24 11:12:33 -0300

    Importado StyleRoot from 'radium' para poder usar media queries Wrapped entire application with StyleRoot so Dynamic changes can be done on App.js Imported Radium from 'radium' on Person.js Added style with media query on Person.js

--- src/App.js
+++ src/App.js
@@ -1,115 +1,111 @@
 import React, { Component } from 'react';
-//Importar radium para poder usarlo
-import Radium from 'radium';
+//Importar StyleRoot para poder usar media queries
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
   state = {
     personaas: [
-      {id:'fa49s8',name: 'Juan', age:25},
-      {id:'a5e46s',name: 'Luis', age: 32},
-      {id:'e7a86',name: 'Lucas', age: 18}
+      { id: 'fa49s8', name: 'Juan', age: 25 },
+      { id: 'a5e46s', name: 'Luis', age: 32 },
+      { id: 'e7a86', name: 'Lucas', age: 18 }
     ]
   };
 
   switchNameHandler = (event, id) => {
-    
+
     const personIndex = this.state.personaas.findIndex(p => {
       return p.id === id;
     });
-    const person = {...this.state.personaas[personIndex]};
+    const person = { ...this.state.personaas[personIndex] };
     person.name = event.target.value;
 
     const persons = [...this.state.personaas];
     persons[personIndex] = person;
     this.setState({
-      personaas: persons 
+      personaas: persons
     })
   }
 
-  deletePersonHandler = (indicePersona) =>{
+  deletePersonHandler = (indicePersona) => {
     const personas = [...this.state.personaas];
     personas.splice(indicePersona, 1);
-    this.setState({personaas: personas});
+    this.setState({ personaas: personas });
   }
 
-  
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow })
   }
 
 
   render() {
-    
+
     const style = {
-      backgroundColor:'green',
-      color:'white',
-      font:'inherit',
-      border:'1px solid blue',
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
       padding: '8px',
-      cursor:'pointer',
-      margin:'auto',
-      //Esta es la sintaxis para usar Radium con cualquier pseudo selector, envolverlos en '' para que sean
-      //string en JSX. el valor es un nuevo objeto JS para que contenga el conjutno de estilos para el estado
-      //cuando hoveree
-      ':hover':{
-        backgroundColor:'lightgreen',
-        color:'black'
+      cursor: 'pointer',
+      margin: 'auto',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
       }
     };
 
     const classes = [];
-    if(this.state.personaas.length <= 2){
+    if (this.state.personaas.length <= 2) {
       classes.push('red');
     }
-    if(this.state.personaas.length <= 1){
+    if (this.state.personaas.length <= 1) {
       classes.push('bold');
     }
 
 
     let persons = null;
-    if (this.state.showPersons){
-      persons =(
+    if (this.state.showPersons) {
+      persons = (
         <div>
-           <p className={classes.join(' ')}>Esto esta funcionando</p>
+          <p className={classes.join(' ')}>Esto esta funcionando</p>
           {
             this.state.personaas.map((persona, indice) => {
-            return (
-             
-              <Person
-              key={persona.id}
-              click={() => this.deletePersonHandler(indice)}
-              name={persona.name} 
-              age={persona.age}
-              changed={(event)=> this.switchNameHandler(event, persona.id)}
-               />
-            );
-          })}
-          </div> 
+              return (
+
+                <Person
+                  key={persona.id}
+                  click={() => this.deletePersonHandler(indice)}
+                  name={persona.name}
+                  age={persona.age}
+                  changed={(event) => this.switchNameHandler(event, persona.id)}
+                />
+              );
+            })}
+        </div>
       );
-      style.backgroundColor= 'red';
-      //Para actualizar el pseudo selector no podemos usar style.:hover sino que tenemos que usar la sintaxis
-      //stile[':nombreDelSelector'] = {}
+      style.backgroundColor = 'red';
       style[':hover'] = {
-        backgroundColor:'salmon',
-        color:'black'
+        backgroundColor: 'salmon',
+        color: 'black'
       };
     }
     return (
-    <div className="App">
-      <button
-        onClick={this.togglePersonsHandler}
-        style={style}
-      >Switch name</button>
-      {persons}
-    </div>
+      //Para poder usar media queries hay que hacer un wrap en la aplicacion con StyleRoot
+      // Estamos usando mediaQUeries para redimensionar el componente Person
+      <StyleRoot>
+        <div className="App">
+          <button
+            onClick={this.togglePersonsHandler}
+            style={style}
+          >Switch name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-// Hacemos este wrap para poder usar las funcionalidades ofrecidas por radium
-//Puede hacerse lo mismo en components que heredan de Component o elementos funciones, imprtando radium y
-//wrapeando el export
 export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 const person = (props)=> {
 
+    //Usamos una media query para redimensionar el componente dinamicamente
+    const style = {
+        '@media-queries (min-width: 500px)':{
+            width: '450px'
+        }
+    }
+
     return(
-        <div className="Person">
+        //asignamos el style con el media query, que sobreescribirá al estilo de su clase
+        <div className="Person" style={style}>
             <p onClick={props.click} >Hola mi nombre es {props.name} y tengo {props.age} años</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
@@ -11,4 +20,4 @@ const person = (props)=> {
     )
 };
 
-export default person;
+export default Radium(person);
