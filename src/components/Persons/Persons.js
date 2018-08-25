689711e16a447c1b928be8364952e2303103d0f2
JuanG <juangiacosabrk@gmail.com>
2018-08-25 16:18:48 -0300

    Implemented componenteWillReceiveProps on Persons.js to understand update lifecycle of a component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -22,6 +22,36 @@ class persons extends Component {
         console.log("[Persons.js] inside componentDidMount");
       }
 
+      //1 Metodo que ejecuta cuando el componente se actualiza
+      componentWillReceiveProps(nextProps){
+          console.log("[UDATE Persons.js] inside componentWillReceiveProps", nextProps);
+      }
+
+      //Segundo metodo que se ejecuta frente a la actualización del componente y antes de volver a llamar a render()
+      //Metodo que nos permite decidir si el componente se actualizará o no. Nos permite ahorrar un poco
+      // de desempeño o performance. 
+      shouldComponentUpdate(nextProps, nextState){
+          console.log("[UPDATE Persons.js] inside shouldComponentUpdate", nextProps, nextState);
+          //DEBE RETORNAR TRUE O FALSE. si retorna true, se actualiza sino se detiene el proceso de
+          // actualizacion sin llegar a volver a llamar al metodo render() para que actualize el DOM
+          return this.props.personas !== nextProps.personas;
+      }
+
+
+      //Tercer metodo que se ejecuta frente a la actualización del componente
+      //Se usa cuando se esta por actualizar el componente
+      componentWillUpdate(nextProps, nextState){
+        console.log("[UPDATE Persons.js] inside componentWillUpdate", nextProps, nextState);
+      }
+
+      // Ultimo metodo que se ejecuta frente a la actualizacion del componente
+      //No tiene nextProps ni nextState porque la actualización ya se llevo a cabo y el estado y las 
+      //props que se actualizaron son las actuales en el componente 
+      componentDidUpdate(){
+        console.log("[UPDATE Persons.js] inside componentDidUpdate");
+      }
+
+
     render() {
         console.log("[Persons.js] inside render")
         return this.props.personas.map((persona, indice) => {
