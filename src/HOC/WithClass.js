776bda5d1fda24d2702d7984e2ed1c6f525776ef
JuanG <juangiacosabrk@gmail.com>
2018-08-26 13:04:18 -0300

    Passed props to WrappedComponent inside withClass.JS using spread operator

--- src/HOC/WithClass.js
+++ src/HOC/WithClass.js
@@ -10,10 +10,15 @@ const withClass = (WrappedComponent, className) => {
     //Retorna una funciona que recibirá las configuraciones como sus props y renderea algo
     //La funcion de retorno califica como un componente funcional. 
 
-    // NUNCA MODIFICAR EL WRAPPED COMPONENT, UNICAMENTE UTILIZARLO AS IS. NO CAMBIARLE EL ESTADO NI LAS PROPS ACA
+    /*  
+        NUNCA MODIFICAR EL WRAPPED COMPONENT, UNICAMENTE UTILIZARLO AS IS. NO CAMBIARLE EL ESTADO NI LAS PROPS ACA PORQUE CADA WRCOM TIENE != PROPS
+        Para pasarle props al componente envuelto se utiliza el operador spread con las props de la funcion, esto le pasa los pares clave-valor
+        al wrappedComponent y nada mas. Una vez hecho esto ya se le pasan las props a cada componente Wrapeado y funciona como si nada. 
+        PASARLE PROPS DE ESTA MANERA CONVIERTE EL HOC EN UN HOC REUSABLE 
+    */
     return (props) => (
         <div className={className}>
-            <WrappedComponent />
+            <WrappedComponent {...props} />
         </div>
     )
 };
