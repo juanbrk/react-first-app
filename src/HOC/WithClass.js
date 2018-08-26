d4161d5126516910cc0aea1d4bf85b9be089f74c
JuanG <juangiacosabrk@gmail.com>
2018-08-26 13:14:42 -0300

    Edited withClass so it returns a stateful component instead of a function

--- src/HOC/WithClass.js
+++ src/HOC/WithClass.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 /* 
     tHIS IS NOT A FUNCTIONAL COMPONENT ITS A NORMAL JS FUNCTION 
     THAT EXPECTS 2 ARGUMENTS AND RETURNS A FUNCTION THAT BEHAVES
@@ -7,19 +7,19 @@ import React from 'react';
     En lugar de pasar props, se le pasan configuraciones. 
 */
 const withClass = (WrappedComponent, className) => {
-    //Retorna una funciona que recibirá las configuraciones como sus props y renderea algo
-    //La funcion de retorno califica como un componente funcional. 
-
-    /*  
-        NUNCA MODIFICAR EL WRAPPED COMPONENT, UNICAMENTE UTILIZARLO AS IS. NO CAMBIARLE EL ESTADO NI LAS PROPS ACA PORQUE CADA WRCOM TIENE != PROPS
-        Para pasarle props al componente envuelto se utiliza el operador spread con las props de la funcion, esto le pasa los pares clave-valor
-        al wrappedComponent y nada mas. Una vez hecho esto ya se le pasan las props a cada componente Wrapeado y funciona como si nada. 
-        PASARLE PROPS DE ESTA MANERA CONVIERTE EL HOC EN UN HOC REUSABLE 
+    /*
+        NO ES NECESARIO QUE ESTE HOC RETORNE UNA FUNCION. 
+        Dependiendo de lo que necesite hacer, puedo hasta retornar un stateful component para, por ejemplo, conectarme con la web
+        Hacemos esto retornando una clase anonima (no tiene nombre). En este caso el HOC es una funcion que retorna una clase on demand
     */
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props} />
-        </div>
-    )
+    return class extends Component {
+        render() {
+            return(
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            );
+        }
+    }
 };
 export default withClass;
